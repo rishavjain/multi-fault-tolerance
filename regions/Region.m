a7df01264d7b0184b4b2c1c48d1056d7f3a520c7
Rishav Jain <rishav.j@gmail.com>
2016-07-08 01:41:22 +0100

    updated naming of Region.m StraightRegion.m turnregion.m, added reverse mapping for StraightRegion.m

--- regions/Region.m
+++ regions/Region.m
@@ -1,12 +1,19 @@
-classdef Region < matlab.mixin.Heterogeneous
+classdef region < matlab.mixin.Heterogeneous
     %REGION Summary of this class goes here
     %   Detailed explanation goes here
     
+    % region: (in direction theta=0)
+    %   s2-----e2
+    %   |      |
+    %   |      |
+    %   s1-----e1
+    
     properties
-        inStartPoint
-        inEndPoint
-        outStartPoint
-        outEndPoint    
+        s1
+        e1
+        s2
+        e2
+        
         type
         area
         polygon
--- regions/StraightRegion.m
+++ regions/StraightRegion.m
@@ -1,7 +1,7 @@
-classdef StraightRegion < Region
+classdef straightregion < region
     %STRAIGHTREGION Summary of this class goes here
     %   Detailed explanation goes here
-    
+        
     properties
         length
         width
@@ -9,58 +9,61 @@ classdef StraightRegion < Region
     end
     
     methods
-        function obj = StraightRegion(inStartPoint, outStartPoint, inEndPoint, outEndPoint)
-            obj.inStartPoint = inStartPoint;
-            obj.inEndPoint = inEndPoint;
-            obj.outStartPoint = outStartPoint;
-            obj.outEndPoint = outEndPoint;
+        function obj = straightregion(s1, s2, e1, e2)
+            obj.s1 = s1;
+            obj.e1 = e1;
+            obj.s2 = s2;
+            obj.e2 = e2;
             
             obj.type = 'straight';
             
-            obj.length = pdist([inStartPoint;inEndPoint]);
-            obj.width = pdist([inStartPoint;outStartPoint]);
+            obj.length = pdist([s1;e1]);
+            obj.width = pdist([s1;s2]);
             
             obj.area = obj.length * obj.width;
-            obj.direction = atan2(inEndPoint(2) - inStartPoint(2), inEndPoint(1) - inStartPoint(1));
+            obj.direction = atan2(e1(2) - s1(2), e1(1) - s1(1));
             
-            obj.polygon = [obj.inStartPoint; obj.inEndPoint; obj.outEndPoint; obj.outStartPoint; obj.inStartPoint];
+            obj.polygon = [obj.s1; obj.e1; obj.e2; obj.s2; obj.s1];
             
-            if pdist([inStartPoint;outStartPoint]) ~= pdist([inEndPoint;outEndPoint])
-                error 'StraightRegion : unequal width'
-            elseif pdist([inStartPoint;inEndPoint]) ~= pdist([outStartPoint;outEndPoint])
-                error 'StraightRegion : unequal length'
+            if abs(pdist([s1;s2]) - pdist([e1;e2])) > 1e-5
+                error 'straightregion : unequal width'
+            elseif abs(pdist([s1;e1]) - pdist([s2;e2])) > 1e-5
+                error 'straightregion : unequal length'
             end
         end
         
-        function [x_, y_] = mapVirtualPoint(obj, x, y, mapping)
-            global VirtualHeight;            
-            xdirection = atan2(obj.inEndPoint(2)-obj.inStartPoint(2), obj.inEndPoint(1)-obj.inStartPoint(1));
+        function [x_, y_] = mapVPt(obj, x, y, mapping)
+            xdirection = atan2(obj.e1(2)-obj.s1(2), obj.e1(1)-obj.s1(1));
             xScale = obj.length / (mapping(2) - mapping(1));
             x = x - mapping(1);
-            x_ = obj.inStartPoint(1) + x*xScale*cos(xdirection);
-            y_ = obj.inStartPoint(2) + x*xScale*sin(xdirection);
+            x_ = obj.s1(1) + x*xScale*cos(xdirection);
+            y_ = obj.s1(2) + x*xScale*sin(xdirection);
             
-            ydirection = atan2(obj.outStartPoint(2)-obj.inStartPoint(2), obj.outStartPoint(1)-obj.inStartPoint(1));
-            yScale = obj.width / VirtualHeight;
-            x_ = x_ + y*yScale*cos(ydirection);
-            y_ = y_ + y*yScale*sin(ydirection);
+            ydirection = atan2(obj.s2(2)-obj.s1(2), obj.s2(1)-obj.s1(1));
+            x_ = x_ + y*cos(ydirection);
+            y_ = y_ + y*sin(ydirection);
         end
         
-        function [x_, y_] = revMapVirtualPoint(obj, x, y, mapping)
-            global VirtualHeight;            
-            xdirection = atan2(obj.inEndPoint(2)-obj.inStartPoint(2), obj.inEndPoint(1)-obj.inStartPoint(1));
+        function [x, y] = revMapVPt(obj, x_, y_, mapping)
             xScale = obj.length / (mapping(2) - mapping(1));
-            x_ = mapping(1) + (x - obj.inStartPoint(1))/(xScale*cos(xdirection));
-            y_ = mapping(1) + (x - obj.inStartPoint(2))/(xScale*sin(xdirection));
+            x = dot(([x_ y_] - obj.s1),(obj.e1 - obj.s1))/sqrt((obj.e1 - obj.s1)*(obj.e1 - obj.s1)');
+            x = (x / xScale) + mapping(1);
             
-            ydirection = atan2(obj.outStartPoint(2)-obj.inStartPoint(2), obj.outStartPoint(1)-obj.inStartPoint(1));
-            yScale = VirtualHeight / obj.width;
-            x_ = x_ + y*yScale*cos(ydirection);
-            y_ = y_ + y*yScale*sin(ydirection);
+            y = dot(([x_ y_] - obj.s1),(obj.s2 - obj.s1))/sqrt((obj.s2 - obj.s1)*(obj.s2 - obj.s1)');
         end
         
-        function handle = drawpolygon(obj)
-            handle = fill(obj.polygon(:,1), obj.polygon(:,2), 'yellow', 'FaceAlpha', 0.2, 'EdgeColor', 'None');
+        function handle = draw_polygon(obj, color, alpha)
+            if ~exist('color', 'var')
+                color = 'w';
+            end
+            
+            if ~exist('alpha', 'var')
+                alpha = 0.2;
+            end
+            
+            handle = fill(obj.polygon(:,1), obj.polygon(:,2), color, 'FaceAlpha', alpha, 'EdgeColor', 'None');
+            plot(obj.polygon(1:2,1), obj.polygon(1:2,2), 'k');
+            plot(obj.polygon(3:4,1), obj.polygon(3:4,2), 'k');
         end
     end
     
